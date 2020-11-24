5d16533c062b8328542736754d3004658455f54f
Tejeswar0475 <tejeswar096@gmail.com>
2020-11-24 15:29:07 +0530

    Added check for attendance

--- empWage.java
+++ empWage.java
@@ -1,10 +1,26 @@
 package empWageDayTwoOne;
 
+import java.util.Random;
 
-class EmployeeWage {
+class EmployeeWage
+{
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to employee wage computation");
-	}
+        public static void main(String[] args)
+        {
+            int present=1;
+            int absent=0;
 
+            System.out.println("Welcome to employee wage computation");
+            Random random=new Random();
+            int  randomNum=random.nextInt(2);
+            if(randomNum == present)
+            {
+                System.out.println("Employee is present:"+randomNum);
+            }
+            else
+            {
+                System.out.println("Employee is absent:"+randomNum);
+            }
+
+        }
 }
