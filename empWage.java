38007f6d9502bba42f8d77e80f175c17e3d05f3d
Tejeswar0475 <tejeswar096@gmail.com>
2020-11-24 19:07:02 +0530

    Added daily employee wage logic

--- empWage.java
+++ empWage.java
@@ -5,22 +5,29 @@ import java.util.Random;
 class EmployeeWage
 {
 
-        public static void main(String[] args)
-        {
-            int present=1;
-            int absent=0;
+	public static void main(String[] args)
+	{
+        	int present=1;
+         	int absent=0;
+		int wagePerHour=20;
+		int fullTimeHour=8;
+		int empWorkHour;
 
-            System.out.println("Welcome to employee wage computation");
-            Random random=new Random();
-            int  randomNum=random.nextInt(2);
-            if(randomNum == present)
-            {
-                System.out.println("Employee is present:"+randomNum);
-            }
-            else
-            {
-                System.out.println("Employee is absent:"+randomNum);
-            }
+		System.out.println("Welcome to employee wage computation");
+		Random random=new Random();
+		int  randomNum=random.nextInt(2);
+		if(randomNum == present)
+            	{
+			empWorkHour=8;
+                	System.out.println("Employee is present:"+randomNum);
+            	}
+            	else
+            	{
+			empWorkHour=0;
+			System.out.println("Employee is absent:"+randomNum);
+		}
+		int dailyEmpWage=empWorkHour*wagePerHour;
+		System.out.println("Daily Emp Wage:"+dailyEmpWage);
 
         }
 }
