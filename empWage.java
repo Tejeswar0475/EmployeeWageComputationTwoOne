3a58b8c90e34f8af96907ae0f61ff4a29361f0ef
Tejeswar0475 <tejeswar096@gmail.com>
2020-11-24 19:46:20 +0530

    Added PartTime employee and wages for types of emp

--- empWage.java
+++ empWage.java
@@ -7,27 +7,34 @@ class EmployeeWage
 
 	public static void main(String[] args)
 	{
-        	int present=1;
-         	int absent=0;
+		int fullTimePresent=2;
+		int partTimePresent=1;
+		int absent=0;
 		int wagePerHour=20;
 		int fullTimeHour=8;
-		int empWorkHour;
+		int partTimeHour=4;
+		int dailyEmpWage=0;
 
 		System.out.println("Welcome to employee wage computation");
 		Random random=new Random();
-		int  randomNum=random.nextInt(2);
-		if(randomNum == present)
+		int randomNum=random.nextInt(3);
+		if(randomNum == fullTimePresent)
+		{
+			System.out.println("Employee is present full time:"+randomNum);
+       	     		dailyEmpWage=fullTimeHour*wagePerHour;
+             		System.out.println("Daily Emp wage:" +dailyEmpWage);
+	    	}
+            	else if(randomNum == partTimePresent )
             	{
-			empWorkHour=8;
-                	System.out.println("Employee is present:"+randomNum);
-            	}
-            	else
-            	{
-			empWorkHour=0;
+                	System.out.println("Employee is present half time:"+randomNum);
+	    		dailyEmpWage=partTimeHour*wagePerHour;
+	    		System.out.println("Daily Emp Wage:"+dailyEmpWage);
+	    	}
+	    	else
+	    	{
 			System.out.println("Employee is absent:"+randomNum);
+        		dailyEmpWage=absent*wagePerHour;
+	      		System.out.println("Employee is absent:" +dailyEmpWage);
 		}
-		int dailyEmpWage=empWorkHour*wagePerHour;
-		System.out.println("Daily Emp Wage:"+dailyEmpWage);
-
         }
 }
